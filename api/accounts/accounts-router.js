ce5fc7d85569ee25ee76c1f4a79aa37925a1b87d
Elizabeth Doyle <123044077+elizabethmdoyle@users.noreply.github.com>
2024-02-23 11:52:58 -0500

    setting up middleware

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -2,26 +2,55 @@ const router = require('express').Router()
 
 router.get('/', (req, res, next) => {
   // DO YOUR MAGIC
+  try{ 
+    res.json('get account')
+  } catch(err) {
+    next({status: 422. message: 'this is horrible'})
+  }
 })
 
 router.get('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+  try{
+    res.json('get account by id')
+  } catch(err) {
+    next(err)
+  }
 })
 
 router.post('/', (req, res, next) => {
   // DO YOUR MAGIC
+   try{
+     res.json('Post account')
+
+  } catch(err) {
+    next(err)
+  }
 })
 
 router.put('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+   try{
+    res.json('update account')
+  } catch(err) {
+    next(err)
+  }
 });
 
 router.delete('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+   try{
+      res.json('delete account')
+  } catch(err) {
+    next(err)
+  }
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
   // DO YOUR MAGIC
+  //error handling middleware where if the routes above throw an error, it redirects them to this router
+   res.status(err.status || res.status(500).json({ message: err.message}))
+
 })
 
 module.exports = router;
--- api/server.js
+++ api/server.js
@@ -2,6 +2,17 @@ const express = require("express");
 
 const server = express();
 
+const accountsRouter = require('../api/accounts/accounts-router');
+
 server.use(express.json());
+
+server.use('/api/accounts', accountsRouter); 
+
+
+server.use('*', (req, res) => {
+    res.status(404).json({
+        message: "endpoint not found"
+    })
+})
 
 module.exports = server;
